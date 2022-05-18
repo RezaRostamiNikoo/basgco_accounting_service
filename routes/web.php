e3738d34bac6d695c5a738c8f3775113944c7c7e
DESKTOP-76J7K0E\Admin <Rezarostaminikoo@gmail.com>
2022-05-18 18:02:36 +0430

    update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 
 
 $router->group(['prefix' => 'accounting'], function () use ($router) {
+    
     $router->group(['prefix' => 'vouchers'], function () use ($router) {
         $router->get('/get', ['uses' => 'TempVoucherController@index']);
         $router->post('/store', ['uses' => 'TempVoucherController@store']);
